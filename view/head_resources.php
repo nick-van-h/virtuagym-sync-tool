6f6e937f32d30a7720c49b72ab5c008a48457c88
Nick <45772524+nick-van-h@users.noreply.github.com>
2022-10-19 18:14:42 +0200

    Initial commit

---
+++ view/head_resources.php
@@ -0,0 +1,7 @@
+<!-- Vendor JS -->
+<script src="<?php public_base_url(); ?>/resources/js/vendor/jquery-3.6.0.js"></script>
+
+<!-- Vendor CSS -->
+<link rel="stylesheet" type="text/css" href="<?php public_base_url(); ?>/resources/css/vendor/reset.css">
+<link rel="stylesheet" type="text/css" href="<?php public_base_url(); ?>/resources/css/vendor/normalize.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
