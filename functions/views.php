320b6310fe4bec3abcc66688cd57995cb2542c7e
Nick <45772524+nick-van-h@users.noreply.github.com>
2022-10-19 18:23:41 +0200

    Initial commit

---
+++ functions/views.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * =========================
+ * Interfaces to website component
+ * =========================
+ */
+
+function get_vw_head_start()
+{
+    include(BASE_PATH . '/views/head_start.php');
+}
+
+function getpart_generic_head_title($title = null)
+{
+    if (empty($title)) {
+        $title = 'VirtuaGym Sync Tool';
+    }
+    echo ('<title>' . $title . '</title>');
+}
+
+function get_vw_head_resources()
+{
+    include(BASE_PATH . '/views/head_resources.php');
+}
+
+function get_vw_head_end()
+{
+    include(BASE_PATH . '/views/head_end.php');
+}
+
+function get_vw_foot_end()
+{
+    include(BASE_PATH . '/views/foot.php');
+}
