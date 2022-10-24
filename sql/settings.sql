79c9c60aa47df3058f838c74eff39a8f13f19d5f
Nick <45772524+nick-van-h@users.noreply.github.com>
2022-10-24 19:10:27 +0200

    Update settings.sql

--- sql/settings.sql
+++ sql/settings.sql
@@ -1,3 +1,5 @@
 CREATE TABLE `u93257p88111_vst`.`settings` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `user_id` INT(11) NOT NULL , `variable` VARCHAR(255) NOT NULL , `value_str` VARCHAR(255) NULL , `value_int` INT(11) NULL , `type` VARCHAR(3) NOT NULL COMMENT 'str/int' , PRIMARY KEY (`id`), INDEX (`user_id`)) ENGINE = InnoDB;
 
 ALTER TABLE `settings` CHANGE `variable` `setting_name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+
+INSERT INTO `settings`(`user_id`, `setting_name`, `value_str`, `type`) VALUES (1,"user_role","admin","str");
