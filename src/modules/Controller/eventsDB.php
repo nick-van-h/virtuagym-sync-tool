e070d88ab774ffa5f3a7331d93e71b553b89b405
Nick <45772524+nick-van-h@users.noreply.github.com>
2022-11-09 19:04:42 +0100

    Iterate

--- src/modules/Controller/eventsDB.php
+++ src/modules/Controller/eventsDB.php
@@ -74,6 +74,45 @@ Class EventsDB extends Database {
         return parent::getRows();
     }
 
+    function getUnsyncedActivities() {
+        //Prepare variables
+        $userid = $this->session->getUserID();
+        $dt = new \DateTime();
+        $dt->modify('-1 month');
+        $mintimestamp = strtotime($dt->format("Y-m-d H:i:s"));
+
+        //Query results
+        $sql = "SELECT DISTINCT 
+                    a.`user_id`,
+                    a.`act_inst_id`,
+                    a.`done`,
+                    a.`deleted`,
+                    a.`act_id`,
+                    a.`event_id`,
+                    a.`timestamp`,
+                    ad.`activity_id`,
+                    ad.`name`,
+                    ad.`deleted` as actdef_deleted,
+                    ad.`club_id`,
+                    ad.`duration`,
+                    ed.`event_start`,
+                    ed.`event_end`,
+                    ed.`attendees`,
+                    ed.`max_attendees`,
+                    ed.`joined`,
+                    ed.`deleted` as evtdef_deleted,
+                    ed.`cancelled`,
+                    ed.`bookable_from`
+                FROM `activities` a
+                LEFT JOIN `act_def` ad ON a.act_id = activity_id
+                LEFT JOIN `evt_def` ed on a.event_id = ed.event_id
+                WHERE a.user_id = (?) AND ed.event_start > (?)
+                ORDER BY ed.event_start DESC"; //TODO NEXT: LEFT ANTI join where act_inst_id not in act_to_cal
+        parent::bufferParams($userid, $mintimestamp);
+        parent::query($sql);
+        return parent::getRows();
+    }
+
     /**
      * =============================================
      * User activities
