b8f0121702bf21f6648c8a58ae058e3bc6e24043
Nick <45772524+nick-van-h@users.noreply.github.com>
2022-11-24 18:28:04 +0100

    Iterate

--- src/modules/Model/Activity.php
+++ src/modules/Model/Activity.php
@@ -3,24 +3,64 @@
 namespace Vst\Interface;
 
 class Activity {
-    private $summary;
-    private $location;
-    private $start;
-    private $end;
-    private $id;
-    private $appointmentId;
+    private $act_inst_id;
+    private $done;
+    private $deleted;
+    private $act_id;
+    private $event_id;
+    private $timestamp;
+    private $activity_id;
+    private $name;
+    private $actdef_deleted;
+    private $club_id;
+    private $duration;
+    private $event_start;
+    private $event_end;
+    private $attendees;
+    private $max_attendees;
+    private $joined;
+    private $evtdef_deleted;
+    private $cancelled;
+    private $bookable_from;
+    private $appointment_id;
+    private $club_name;
+    private $full_address;
+    private $street;
+    private $zip_code;
+    private $city;
+    private $club_description;
 
     private const REQUIRED_PARAMETERS = array('summary','location','start','end','id','appointment_id');
 
-    public function __construct(array $params[] = null) {
-        //Set parameters if passed
+    public function __construct() {
+        $arguments = func_get_args();
+        $this->setParameters($arguments);
     }
 
-    public function setParameters(array $params[]) {
+    public function setParameters() {
+        $arguments = func_get_args();
 
+        if(!empty($arguments)) {
+            foreach($arguments[0] as $key => $value) {
+                if(property_exists($this, $key)) {
+                    $this->{$key} = $value;
+                }
+            }
+        }
     }
 
-    public function getRequiredParameters() {
+    public function getParameters() {
+        $arguments = func_get_args();
 
+        $ret = [];
+
+        if(!empty($arguments)) {
+            foreach($arguments[0] as $key => $value) {
+                if(property_exists($this, $key)) {
+                    $ret[$key] = $value;
+                }
+            }
+        }
+        return $ret;
     }
 }
